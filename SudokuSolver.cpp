08329bf6285892332d14791ed81271628cb05b1c
Anshuman Jindal <jindalanshuman2004@gmail.com>
2024-07-04 15:46:50 +0530

    second commit

--- SudokuSolver.cpp
+++ SudokuSolver.cpp
@@ -3,12 +3,13 @@ using namespace std;
 
 int grid[9][9]={{0}};
 #define N 9
+#define M 3
 
 class Solution{
 public:
     bool row[N][N+1]={{0}};
     bool col[N][N+1]={{0}};
-    bool sq[3][3][N+1]={{{0}}};
+    bool sq[M][M][N+1]={{{0}}};
     vector<pair<int, int>>zero;
     bool b=0;
     bool SolveSudoku(){
@@ -17,11 +18,11 @@ public:
         for(int i=0; i<N; i++){
             for(int j=0; j<N; j++){
                 int x=grid[i][j];
-                if(row[i][x]||col[j][x]||sq[i/3][j/3][x]){
+                if(row[i][x]||col[j][x]||sq[i/M][j/M][x]){
                     cout<<"This Sudoku is not solvable\n";
                     return 0;
                 }
-                if(x!=0)row[i][x]=col[j][x]=sq[i/3][j/3][x]=1;
+                if(x!=0)row[i][x]=col[j][x]=sq[i/M][j/M][x]=1;
                 else zero.push_back({i, j});
             }
         }
@@ -42,11 +43,11 @@ public:
         }
         int x=zero[ind].first, y=zero[ind].second;
         for(int i=1; i<=N; i++){
-            if(!row[x][i] && !col[y][i] && !sq[x/3][y/3][i]){
+            if(!row[x][i] && !col[y][i] && !sq[x/M][y/M][i]){
                 grid[x][y]=i;
-                row[x][i]=col[y][i]=sq[x/3][y/3][i]=1;
+                row[x][i]=col[y][i]=sq[x/M][y/M][i]=1;
                 solve(grid, ind+1);
-                row[x][i]=col[y][i]=sq[x/3][y/3][i]=0;
+                row[x][i]=col[y][i]=sq[x/M][y/M][i]=0;
                 if(b)return;
             }
         }
@@ -63,14 +64,22 @@ public:
 
 int main(){
     cout<<"Enter Sudoku. For empty squares, enter 0\n";
-    for(int i=0; i<9; i++){
-        for(int j=0; j<9; j++){
-            char c;
-            cin>>c;
-            if(c>='0' && c<='9')grid[i][j]=c-'0';
+    for(int i=0; i<N; i++){
+        for(int j=0; j<N; j++){
+            string s;
+            cin>>s;
+            for(int i=0; i<s.size(); i++){
+                if(s[i]<'0'||s[i]>'9'){
+                    cout<<"Enter a valid number\n";
+                    j--;
+                    continue;
+                }
+            }
+            if(stoi(s)>=0 && stoi(s)<=N)grid[i][j]=stoi(s);
             else{
                 cout<<"Enter a valid number\n";
                 j--;
+                continue;
             }
         }
     }
